b11c313e6daf7a7a7f0bade61d71956a03853691
reinerwaldmann <mksd@list.ru>
2014-05-20 11:41:29 +0400

    улучшена формула линеаризации, но уточнение так и не родилось.

--- resistors.py
+++ resistors.py
@@ -1,16 +1,37 @@
+
+#Все формулы получены по статье про транзисторы и
+# Теория вероятностей.  Вентцель Е.С.
+#4-е изд., стереотип. - М.: Наука, Физматгиз, 1969 - 576 с.
+
 from cmath import sqrt
 import numpy as np
-
-
 import matplotlib.pyplot as plt
-
-import scipy as scp
 import random
-
 import sympy as smp
 
-
 __author__ = 'reiner'
+
+
+def derivsym (funcstr, argseq):
+    """
+    funcstr - строка, являющая собою функцию
+    argseq - последовательность из аргументов, заданных как строки
+    """
+
+    res=list()
+    for arg in argseq:
+        res.append(str(smp.diff(funcstr,arg)))
+
+    return res
+
+
+def evalderivsymv (funcstr, argseq, arginitseq ):
+    """
+    arginitseq - locals
+    derivseq - gave by derivssym func
+    """
+    derivseq=derivsym (funcstr, argseq)
+    return list (map (lambda x: eval(x,arginitseq) , derivseq))
 
 
 #returns u2, u3
@@ -20,15 +41,15 @@ def funct(u1, r1, r2, r3):
 #print (smp.diff("u1* (r2+r3)/(r1+r2+r3)", "r2"))
 
 def derivate(u1, r1, r2, r3):
-    rsum=(r1+r2+r3)**2
-    du3dr1 = -1*u1*r3/rsum
-    du3dr2 = -1*u1*r3/rsum
-    du3dr3 = (r1+r2)*u1/rsum
+     rsum=(r1+r2+r3)**2
+     du3dr1 = -1*u1*r3/rsum
+     du3dr2 = -1*u1*r3/rsum
+     du3dr3 = (r1+r2)*u1/rsum
 
-    du2dr1 = -1*u1*r2/rsum+du3dr1
-    du2dr2 = (r1+r3)*u1/rsum+du3dr2
-    du2dr3 = -1*u1*r2/rsum+du3dr3
-    return (du2dr1, du2dr2, du2dr3), (du3dr1, du3dr2, du3dr3)
+     du2dr1 = -1*u1*r2/rsum+du3dr1
+     du2dr2 = (r1+r3)*u1/rsum+du3dr2
+     du2dr3 = -1*u1*r2/rsum+du3dr3
+     return (du2dr1, du2dr2, du2dr3), (du3dr1, du3dr2, du3dr3)
 
 
 
@@ -50,7 +71,7 @@ def countDispLinearization (u1, r1, r2, r3, V,  derivatefunc):
     for i in range (0, 3):
         for j in range (0, 3):
             if (i<j):
-                sumsecMU2+=deriv[0][i]*deriv[0][j]*V[i,j]
+                sumsecMU2+=2*deriv[0][i]*deriv[0][j]*V[i,j]
 
 
 
@@ -58,7 +79,7 @@ def countDispLinearization (u1, r1, r2, r3, V,  derivatefunc):
     for i in range (0, 3):
         for j in range (0, 3):
             if (i<j):
-                sumsecMU3+=deriv[1][i]*deriv[1][j]*V[i,j]
+                sumsecMU3+=2*deriv[1][i]*deriv[1][j]*V[i,j]
 
     return first_memberU2+sumsecMU2, first_memberU3+sumsecMU3
     #return first_memberU2, first_memberU3
@@ -103,16 +124,13 @@ def generrandvals (M_ksi, cov_ksi, nvol=1000):
         tseq1.append(ksi[ni][0])
         tseq2.append(ksi[ni][1])
         tseq3.append(ksi[ni][2])
-
     XX=np.array([tseq1, tseq2, tseq3])
     COVTEST=np.cov(XX, bias=-1)
 
     #print ("Mean Values:")
     #print (np.mean(tseq1), np.mean(tseq2), np.mean(tseq3)  )
-
    # print ("Cov matrix:")
     #print (COVTEST)
-
     #plt.hist(tseq1, 100, label="tseq1")
     #plt.xlabel('Smarts')
     #plt.ylabel('Probability')
@@ -145,6 +163,14 @@ def countDispMonteKarlo1 (u, sectuple, func):
     return countDispMonteKarlo(u, sectuple[0], sectuple[1], sectuple[2], func)
 
 
+func1 = "u1* (r2+r3)/(r1+r2+r3)"
+func2= "u1* r3/(r1+r2+r3)"
+argseq=["r1", "r2", "r3" ]
+
+
+def derivate1 (u1, r1, r2, r3):
+    arginitseq={"u1":u1, "r1":r1, "r2":r2, "r3":r3}
+    return evalderivsymv(func1, argseq, arginitseq), evalderivsymv(func2, argseq, arginitseq)
 
 
 def test1():
@@ -167,16 +193,12 @@ def test1():
 def test2(iM, iV, nvol):
     rndv=generrandvals(iM, iV, nvol)
     truedisp=countDispMonteKarlo1(100, rndv ,funct)
-#    lineardisp=countDispLinearization(100, rndv[4], rndv[5], rndv[6], rndv[3], derivate)
-    lineardisp=countDispLinearization(100, iM[0], iM[1], iM[2], iV, derivate)
-
+    lineardisp=countDispLinearization(100, iM[0], iM[1], iM[2], iV, derivate1)
     print ("true", "linear", "diff")
-    print (truedisp[1], lineardisp[0], 100*(truedisp[1]-lineardisp[0])/truedisp[1], "%" )
-    print (truedisp[3], lineardisp[1], 100*(truedisp[3]-lineardisp[1])/truedisp[3], "%" )
-
-
-
-
+    diff1=100*(truedisp[1]-lineardisp[0])/truedisp[1]
+    diff2=100*(truedisp[3]-lineardisp[1])/truedisp[3]
+    print (truedisp[1], lineardisp[0], "%5.2f"%diff1, "%" )
+    print (truedisp[3], lineardisp[1], "%5.2f"%diff2, "%" )
 
 
 V=np.array       ( [[4, 2, 3],
@@ -197,20 +219,16 @@ M=np.array([20,30,400])
 #for x in [100, 1000, 10000]:
 #    test2(M,V,x)
 
+print ("Матожидание 10 30 400")
+test2(M,V,100000)
+M=np.array([20,30,40])
+print ("Матожидание 10 30 40")
+test2(M,V,100000)
 
 
 
 
-#testing sympy
-
-#def funct(u1, r1, r2, r3):
-#    return u1* (r2+r3)/(r1+r2+r3), u1* r3/(r1+r2+r3)
-
-print (smp.diff("u1* (r2+r3)/(r1+r2+r3)", "r2"))
-
-
-
-# for r1 in range (10,15):
+#for r1 in range (10,15):
 #      for r2 in range (100,200,10):
 #          for r3 in range (200,205):
 #              M=np.array([r1, r2, r3])
