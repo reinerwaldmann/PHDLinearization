fa20aad73e6e0600c1e0fbd4b20c71292361679a
Reiner <mksd@list.ru>
2015-03-26 13:13:24 +0300

    ++dfsdf122

--- Cases/Dependency/Dependency.py
+++ Cases/Dependency/Dependency.py
@@ -5,14 +5,13 @@ __author__ = 'vasilev_is'
 import pickle
 
 import numpy as np
+import Ofiura.Ofiura_general as o_g
 
 
 #конкретно к модели диода относящиеся вещи
 
 btrue = [1.238e-14, 1.8, 100]
-print (np.linalg.norm(btrue))
-
-
+#print (np.linalg.norm(btrue))
 
 
 def makeplan_l (plantype, n ):
@@ -25,21 +24,29 @@ def makeplan_l (plantype, n ):
     else:
         pass
 
-
-
-
-
-
-
-
-def makebinit_lambda (lbinitbtrue):
-    #- делает начальное значение в зависимости от параметров lbinitbtrue
+def makebinit_lambda (bstart, bend):
     global btrue
+    #- делает начальное значение в зависимости от параметров lbinitbtrue
+    binit = o_g.uniformVector(bstart, bend)
+    return binit, np.linalg.norm(np.array(binit)-np.array(btrue))
+
 
 
 def makediap_lambda (diapwidth, assym):
     #- делает диапазон в зависимости от параметров diapwidth, assym
     global btrue
+
+    btrue1=np.array(btrue)
+
+    bcenter = np.array(btrue) * (1+np.linalg.norm(btrue)/assym)
+
+    print (np.linalg.norm(btrue1)/assym)
+
+    bstart, bend = bcenter* (1-np.linalg.norm(bcenter)/(2*diapwidth)), bcenter* (1+np.linalg.norm(bcenter)/(2*diapwidth))
+    return bstart, bend
+
+print (makediap_lambda (0.1, 0.2))
+
 
 
 def makeVe_lambda(detVe):
@@ -123,16 +130,12 @@ def mainfunc (i_plantype, i_n, i_lbinitbtrue, i_diapwidth, i_assym, i_nsiggen, i
         btrue фиксировано. Всё остальное определяется, отталкиваясь от него
     """
     res  =  list()
-
     resfolder= 'Results'
-
     #проверка доступности папки записи результатов
     with open (resfolder+'/'+'test.txt', 'wt')  as file:
         file.write('test')
 
     print (locals()) #чтоб не расслабляться при чтении логов
-
-
 
     for plantype in i_plantype:
         for n in i_n:
@@ -144,9 +147,15 @@ def mainfunc (i_plantype, i_n, i_lbinitbtrue, i_diapwidth, i_assym, i_nsiggen, i
                                 for detVe in i_detve:
                                     for isbinitgood in i_isbinitgood:
 
+                                        binit=bb[0]
+                                        plan = makeplan_lambda(plantype, n)
+                                        bstart, bend = makediap_lambda(diapwidth, assym)
+                                        Ve = makeVe_lambda (detVe)
+                                        bb=makebinit_lambda(bstart, bend)
+
                                         condition = {'plantype': plantype,
                                                      'n':n,
-                                                     'lbinitbtrue':lbinitbtrue,
+                                                     'lbinitbtrue':bb[1],
                                                      'diapwidth':diapwidth,
                                                      'assym':assym,
                                                      'nsiggen':nsiggen,
@@ -154,10 +163,6 @@ def mainfunc (i_plantype, i_n, i_lbinitbtrue, i_diapwidth, i_assym, i_nsiggen, i
                                                      'detve':detVe,
                                                      'isbinitgood':isbinitgood}
 
-                                        plan = makeplan_lambda(plantype, n)
-                                        binit = makebinit_lambda(lbinitbtrue)
-                                        bstart, bend = makediap_lambda(diapwidth, assym)
-                                        Ve = makeVe_lambda (detVe)
 
                                         if detVe<10e-15:  #считаем Ve практически нулевой, разброс - отсутствующим
                                             result = gknuxfunc_lambda (plan, binit, bstart, bend, Ve)
