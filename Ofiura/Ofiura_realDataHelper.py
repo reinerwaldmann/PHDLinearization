f1bd083c587d4b806996eb9876bcf6a53fb83cf1
Reiner <mksd@list.ru>
2015-06-08 14:33:06 +0300

    добавил хелпер

---
+++ Ofiura/Ofiura_realDataHelper.py
@@ -0,0 +1,37 @@
+__author__ = 'vasilev_is'
+import os
+#функция, которая считывает имя файла в measdata
+
+def readMeasdata (infilename):
+    rs=list()
+
+    with open (infilename) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        try:
+            bl=line.split(',')
+            x=float(bl[0].strip())
+            y=float(bl[1].strip())
+            rs.append( {'x':x, 'y':y } )
+
+        except:
+            pass
+    if not rs:
+        print ('empty'+infilename)
+    return rs
+
+
+def grfiles (folder):
+    rs=list() #то будет список списков
+    for file in os.listdir(folder):
+        if file.endswith('.txt'):
+            rs.append(readMeasdata(folder+file))
+    return rs
+
+
+def test ():
+    #print (readMeasdata('D:\PHd\RealDiode\\1N4004\Reports\\adv\\01.txt'))
+    print (grfiles('D:\PHd\RealDiode\\1N4004\Reports\\adv\\'))
+
+
