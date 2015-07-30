422a661c7229818bd289e9ec763a072f2aa66f28
Reiner <mksd@list.ru>
2015-07-30 14:17:38 +0300

    пилим estimators

--- Fianora/Fianora_Estimators.py
+++ Fianora/Fianora_Estimators.py
@@ -1,1 +1,582 @@
 __author__ = 'vasilev_is'
+
+import  Fianora.Fianora_Models
+import numpy as np
+import platform
+import copy
+import math
+
+
+class Options():
+    """
+    Estimation process options
+    """
+
+    def __getattr__(self, item):
+        return None
+
+
+    # NSIG = options['NSIG']
+    #
+    #     NSIGGENERAL = options['NSIGGENERAL']
+    #
+    #     implicit = options['implicit']
+    #
+    #     verbose = options['verbose']
+    #
+    #     verbose_wrapper = options['verbose_wrapper']
+    #
+    #     isBinitGood = options['isBinitGood']
+
+
+class AbstractEstimator():
+    def __init__(self, bstart, bend, binit, xstart, xend, model, measdata, Ve):
+        # вопрос, удастся ли провернуть позднее связывание
+        self.bstart = bstart
+        self.bend = bend
+        self.binit = binit
+        self.xstart = xstart
+        self.xend = xend
+        self.model = model
+        self.measdata = measdata
+        self.Ve = Ve
+
+    def estimate (self, options = None):
+        """
+        Шаблонный метод, идея в том, что сначала выполняется оценка, потом определяются показатели адекватности
+        в итоге получается некоторый стандартный словарь
+        :return:
+        """
+        z = self.estimate_method(options).copy()
+        z.update(self.adequacy(z))
+        z['name'] = self.model.name
+
+        return z
+
+    def estimate_method(self, options):
+        """
+        Estimation method, like gknux
+        :return:
+        """
+        raise NotImplementedError ("Pleas implement me")
+
+    def adequacy(self, est):
+        """
+        Adequacy determiner
+        Спецификация:
+        Тип данных - словарь
+        Поля и описание -
+         'b'            оцененный вектор коэффициентов
+         'numiter'      число итераций
+         'log'          лог
+         'Sklist'       список значений объектной функции
+         'Sk'           последнее значение объектной функции
+
+         'Vb'           ковариационная матрица оценённого вектора b
+         'VbSigmas'     сигмы из ковариационной матрицы оценённого вектора b (корни квадратные из диагонали)
+
+
+         'AvLogTruth'   среднее по точкам эксперимента значение логарифма правдоподобия
+         'DispLT'       дисперсия логарифма правдоподобия
+         'SigmaLT'      среднеквадратическое отклонение логарифма правдоподобия
+         'AvDif'        средний остатков
+         'DispDif'      дисперсия остатков
+         'SigmaDif'     СКВ остатков
+         'Diflist'      список остатков
+         'name'         название метода
+
+
+        """
+
+        rs={}
+
+
+        Vb = rs['Vb'] = self.countVbForMeasdata(est['b'])
+
+        sigmas=list()
+        for i in range (Vb.shape[0]):
+            sigmas.append(math.sqrt(Vb[i][i]))
+        rs['VbSigmas'] = sigmas
+
+        names=['AvLogTruth','DispLT', 'SigmaLT', 'AvDif', 'DispDif', 'SigmaDif', 'Diflist']
+        values = list(logTruthness (measdata, b, Ve,  func, c))+list(averageDif(measdata, b, Ve,  func, c))
+        return dict(zip (names, values))
+
+        return rs
+
+
+
+    def countVbForMeasdata(self, b:list):
+        """
+        Для неявных функций актуальным является значение y. В некоторых случаях (напр. при последовательном планировании)
+        y уже просчитан, и нет нужды его считать снова, задавая функцию.
+        :param expplan: план эксперимента
+        :param b: b (вектор коэффициентов)
+        :param b: b (вектор коэффициентов)
+        :param c: словарь доп. параметров
+        :param Ve: ковариационная матрица ошибок экспериментов np.array
+        :param jac: функция якобиана (на входе x,b,c=None, y=None), возвращать должно np.array
+        :param measdata: данные измерений
+        :return: значение определителя для данного плана эксперимента
+        """
+        G=np.zeros((len(b),len(b))) #матрица G
+
+        for point in measdata:
+            jj=jac(point['x'], b, c, point['y'])
+            #G+=jj*np.linalg.inv(Ve)*jj.T
+
+            #print  (jj.T, np.linalg.inv(Ve), jj)
+
+            G+=np.dot ( np.dot(jj.T, np.linalg.inv(Ve)), jj)
+
+            #G+=np.dot(jj.T, jj)
+        try:
+            return np.linalg.inv(G)
+        except BaseException as e:
+            print('Fatal error in countVbForMeasdata: ',e)
+            print('b vector=',b)
+            print('current point=',point)
+            print('G=',G)
+            exit(0)
+
+
+
+
+
+        def logTruthness (measdata:list, b:list, Ve,  func, c):
+        """
+        Считает логарифм функции правдоподобия для известной ковариационной матрицы Ve - ошибок экспериментальных данных
+        :param measdata: список словарей экспериментальных данных [{'x': [] 'y':[])},{'x': [] 'y':[])}]
+        :param b: оценка вектора коэффициентов
+        :param Ve: ковариационная матрица ошибок экспериментальных данных
+        :param func: callable функция x,b,c, возвращает значение y
+        :param c: словарь дополнительных переменных
+        :return: среднее значение логарифма функции правдоподобия, дисперсию по выборке экспериментальных данных, стандартное отклонение
+        """
+        S=list()
+
+        for i in range(len(measdata)):
+            measpoint = measdata[i]
+            dif=np.array(measpoint['y'])-np.array(func(measpoint['x'],b,c))
+
+            S.append(np.dot(np.dot(dif.T, np.linalg.inv(Ve)), dif))
+
+        K=Ve.shape[0] #число откликов
+        M=len(b) #число коэффициентов
+        N=len(measdata)
+        shift=K*N/(K*N-M)
+
+
+        Snp=list(map(np.float64, S))
+
+        # for item in S:
+        #     item = np.float96(item)
+        # print (S)
+
+        #print (S)
+        #print (Snp)
+
+
+        Average=np.average(Snp)*shift
+        Disp = np.var(S)*shift*shift
+
+        return Average, Disp, math.sqrt(Disp)
+
+        def averageDif(measdata:list, b:list, Ve,  func, c):
+        """
+        :param measdata: список словарей экспериментальных данных [{'x': [] 'y':[])},{'x': [] 'y':[])}]
+        :param b: вектор коэфф
+        :param Ve:  Ve ковар. матрица измеренных данных
+        :param funcf callable функция, параметры по формату x,b,c
+        :param c словарь дополнительных постоянных
+        :return: среднее, дисперсия, стандартное отклонение
+        """
+
+        diflist=list()
+        diflistNoAbs=list()
+
+
+        for measpoint in measdata:
+            dif=np.array(measpoint['y'])-func(measpoint['x'],b,c)
+
+            diflist.append(np.abs(dif ))
+            diflistNoAbs.append(dif)
+
+            diflistn=list(map(np.float64, diflist))
+            diflistna=list(map(np.float64, diflistNoAbs))
+
+
+        return np.average(diflistn), np.var(diflistn), math.sqrt(np.var(diflistn)), diflistna
+
+
+
+
+def logTruthness (measdata:list, b:list, Ve,  func, c):
+    """
+    Считает логарифм функции правдоподобия для известной ковариационной матрицы Ve - ошибок экспериментальных данных
+    :param measdata: список словарей экспериментальных данных [{'x': [] 'y':[])},{'x': [] 'y':[])}]
+    :param b: оценка вектора коэффициентов
+    :param Ve: ковариационная матрица ошибок экспериментальных данных
+    :param func: callable функция x,b,c, возвращает значение y
+    :param c: словарь дополнительных переменных
+    :return: среднее значение логарифма функции правдоподобия, дисперсию по выборке экспериментальных данных, стандартное отклонение
+    """
+    S=list()
+
+    for i in range(len(measdata)):
+        measpoint = measdata[i]
+        dif=np.array(measpoint['y'])-np.array(func(measpoint['x'],b,c))
+
+        S.append(np.dot(np.dot(dif.T, np.linalg.inv(Ve)), dif))
+
+    K=Ve.shape[0] #число откликов
+    M=len(b) #число коэффициентов
+    N=len(measdata)
+    shift=K*N/(K*N-M)
+
+
+    Snp=list(map(np.float64, S))
+
+    # for item in S:
+    #     item = np.float96(item)
+    # print (S)
+
+    #print (S)
+    #print (Snp)
+
+
+    Average=np.average(Snp)*shift
+    Disp = np.var(S)*shift*shift
+
+    return Average, Disp, math.sqrt(Disp)
+
+def averageDif(measdata:list, b:list, Ve,  func, c):
+    """
+    :param measdata: список словарей экспериментальных данных [{'x': [] 'y':[])},{'x': [] 'y':[])}]
+    :param b: вектор коэфф
+    :param Ve:  Ve ковар. матрица измеренных данных
+    :param funcf callable функция, параметры по формату x,b,c
+    :param c словарь дополнительных постоянных
+    :return: среднее, дисперсия, стандартное отклонение
+    """
+
+    diflist=list()
+    diflistNoAbs=list()
+
+
+    for measpoint in measdata:
+        dif=np.array(measpoint['y'])-func(measpoint['x'],b,c)
+
+        diflist.append(np.abs(dif ))
+        diflistNoAbs.append(dif)
+
+        diflistn=list(map(np.float64, diflist))
+        diflistna=list(map(np.float64, diflistNoAbs))
+
+
+    return np.average(diflistn), np.var(diflistn), math.sqrt(np.var(diflistn)), diflistna
+
+
+
+
+
+
+
+
+
+
+
+
+
+class NGEstimator(AbstractEstimator):
+
+    def makeSkInit (self):
+        measdata = self.measdata
+        funcf = self.model.funcf
+        binit = self.binit
+        Sk=None
+        for point in measdata:
+            fxbc=funcf(point['x'],binit)
+            if fxbc is None:
+                print ("ERROR: makeSkInit: funcf returned None")
+                return None
+            dif=point['y']-fxbc
+            if Sk is None:
+                Sk=np.dot(dif.T,dif)
+            else:
+                Sk+=np.dot(dif.T,dif)
+            return Sk
+
+    def islinux (self):
+        if 'Windows' in platform.system():
+            return False
+        return True
+
+
+    def makeAinit (self,  isBinitGood=True):
+        """
+        расчёт ведётся по правилу "binit есть хорошее предположение в рамках области"
+        :param bstart:
+        :param bend:
+        :param Skbinit:
+        :param binit:
+        :return:
+        """
+        bstart = self.bstart
+        binit = self.binit
+        bend = self.bend
+        Skbinit = self.makeSkInit()
+
+        A=np.zeros ( (len(binit), 2 ))  #,  dtype=np.float96 if islinux() else np.float64
+
+        for i in range (len(binit)):
+            #A[i][0]=A[i][1]=.001*(bend[i]-bstart[i])*Skbinit  #универсально
+
+            if isBinitGood:
+                A[i][0] = 0.001*(binit[i]-bstart[i])*Skbinit
+                A[i][1] = 0.001*(bend[i]-binit[i])*Skbinit
+            else:
+                A[i][0] = A[i][1] = 0.001*(bend[i]-bstart[i])*Skbinit  #универсально
+        return A
+
+    def countSklims(self, A, b):
+        bstart = self.bstart
+        bend = self.bend
+        partone=parttwo=.0
+        for i in range (len(b)):
+            partone+=A[i][0]/(b[i]-bstart[i]) #UNSAFE: если вдруг, ну вдруг b=bstart или bend, то будет <strike> треш </strike> креш с делением на ноль.
+            parttwo+=A[i][1]/(bend[i]-b[i])
+        return partone+parttwo
+
+    def countN (self, A, b):
+        bstart = self.bstart
+        bend = self.bend
+
+        N=np.zeros ((len(b),len(b)))
+        for j in range (len(b)):
+            partone=2*A[j][0]/(b[j]-bstart[j])**3
+            parttwo=2*A[j][1]/(bend[j]-b[j])**3
+            N[j][j]+=parttwo+partone #так как матрица нулевая
+        return N
+
+
+    def estimate_method(self, options):
+        binit = self.binit
+
+        NSIG = options.NSIG if options.NSIG else 50
+        NSIGGENERAL = options.NSIGGENERAL if options.NSIGGENERAL else 50
+        verbose = options.verbose if options.verbose else False
+        verbose_wrapper = options.verbose_wrapper if options.verbose_wrapper else False
+        isBinitGood = options.isBinitGood if options.isBinitGood else False
+
+
+
+        if isinstance(self.model, Fianora.Fianora_Models.ImplicitModel): #если модель - это подкласс неявной модели, то...
+            implicit = True
+        else:
+         """
+        Обёртка для grandCountGN_UltraX1_Limited для реализации общего алгоритма
+        :param funcf callable функция, параметры по формату x,b,c
+        :param jacf callable функция, параметры по формату x,b,c,y
+        :param measdata:list список словарей экспериментальных данных [{'x': [] 'y':[])},{'x': [] 'y':[])}]
+        :param binit:list начальное приближение b
+        :param bstart:list нижняя граница b
+        :param bend:list верхняя граница b
+        :param c словарь дополнительных постоянных
+        :param A матрица коэффициентов a
+        :param NSIG=3 точность (кол-во знаков после запятой)
+        :param implicit True если функция - неявная, иначе false
+        :param verbose Если True, то подробно принтить результаты итераций
+        :returns b, numiter, log - вектор оценки коэффициентов, число итераций, сообщения
+        """
+
+        maxiter=100
+        b,bpriv=binit,binit
+        gknux=None
+        gknuxlist=list()
+
+        A=self.makeAinit(isBinitGood)
+
+        log=''
+
+        if verbose_wrapper:
+            print ('==grandCountGN_UltraX1_Limited_wrapper is launched==\n\n')
+
+        for numiter in range (maxiter):
+            bpriv=copy.copy(b)
+
+            gknux=self.grandCountGN_UltraX1_Limited (A, NSIG, implicit, verbose) #посчитали b
+
+
+            if gknux is None:
+                print ("grandCountGN_UltraX1_Limited_wrapper crashed on some iteration")
+                return None
+            gknuxlist.append(gknux)
+            if verbose_wrapper:
+                print ('Iteration \n',numiter,'\n' ,gknux)
+
+            b=gknux['b']
+
+
+            if not gknux[2]=='':
+                #log+="On gknux iteration "+numiter+": "+ gknux[2]+"\n"
+                log+="On gknux iteration {0}: {1}\n".format (numiter, gknux[2])
+
+
+            for j in range (len(binit)): #уменьшили в два раза
+                A[j][0]*=0.5
+                A[j][1]*=0.5
+
+            condition=False
+            for i in range (len(b)):
+                if math.fabs ((b[i  ]-bpriv[i])/bpriv[i]) > math.pow(10,-1*NSIGGENERAL):
+                    condition=True
+            if not condition:
+                break
+
+            #мол если хоть один компонент вектора b значимо изменился, тогда продолжать. Иначе программа дойдёт до break и цикл прекратится
+
+        if verbose_wrapper:
+            print ('grandCountGN_UltraX1_Limited_wrapper iterations number:', numiter)
+            print (gknux, log)
+
+        gknux['log'] = log
+        return gknux
+
+
+    def grandCountGN_UltraX1_Limited (self, A, _NSIG=3, implicit=False, verbose=False):
+        """
+        Производит оценку коэффициентов по методу Гаусса-Ньютона с переменным шагом с ограничениями, заданными диапазоном
+        В стандартный поток вывода выводит отладочную информацию по каждой итерации
+
+
+        :param NSIG=3 точность (кол-во знаков после запятой)
+        :param implicit True если функция - неявная, иначе false
+        :param verbose Если True, то подробно принтить результаты итераций
+        :returns b, numiter, log - вектор оценки коэффициентов, число итераций, сообщения
+        """
+        funcf = self.model.funcf
+        jacf = self.model.jacf
+        measdata = self.measdata
+        binit = self.binit
+
+        #sign - если  1, то b=b+deltab*mu, иначе b=b-deltab*mu. При неявной функции надо ставить sign=0
+        sign=0 if implicit else 1
+
+        Sklist=list()
+        b=binit
+        log=""
+        numiter=0
+        condition=True
+        while (condition):
+            m=len(b) #число коэффициентов
+            G=np.zeros((m,m))
+            B5=None
+            bpriv=copy.copy(b)
+            Sk=0
+            for point in measdata:
+                jac=jacf(point['x'],b)
+
+                if jac is None:
+                    return None
+                G+=np.dot(jac.T,jac)
+
+                try:
+                    dif=np.array(point['y'])-np.array(funcf(point['x'],b))
+                except BaseException as e:
+                    if verbose:
+                        print('grandCountGN_UltraX1_limited: As funcf returned None, method  stops:', e)
+                        print (point, b, sep='\n')
+                    return None
+
+
+                if B5 is None:
+                    B5=np.dot(dif, jac)
+                else:
+                    B5+=np.dot(dif,jac)
+                Sk+=np.dot(dif.T,dif)
+
+
+            N=self.countN(A,b)
+            Sklims=self.countSklims(A,b)
+
+            #G=G-N if implicit else G+N #добавляем градиент от штрафных функций
+            G=G+N
+            Sk+=Sklims #добавиляем объектную функцию от штрафных функций
+
+            #print(np.linalg.inv(G), B5[:,0])
+            #костыль для диодной задачи
+            if hasattr(B5, 'A1'):
+                B5=B5.A1
+            try:
+                deltab=np.dot(np.linalg.inv(G), B5)
+            except BaseException as e:
+                if verbose:
+                    print('Error in G:', e)
+                    print('G=',G)
+                return None
+
+
+            #mu counting
+            mu=4
+            cond2=True
+            it=0
+            Skmu=0
+            while (cond2):
+                Skmu=self.countSklims(A,b) #добавляем объектную функцию от штрафных функций
+                mu/=2
+                for point in measdata:
+                    try:
+                        dif=np.array(point['y'])-np.array(funcf(point['x'],b+deltab*mu)) if sign else np.array(point['y'])-np.array(funcf(point['x'],b-deltab*mu))
+                    except:
+                        continue
+
+                    Skmu+=np.dot(dif.T, dif)
+
+                it+=1
+
+
+                if (it>200):
+                    log+="Mu counting: break due to max number of iteration exceed"
+                    break
+                cond2=Skmu>Sk
+
+            b=b+deltab*mu if sign else b-deltab*mu
+
+            Sk=Skmu
+
+            Sklist.append(Sk)
+            if verbose:
+                print ("Sk:",Sk)
+                print ("Iteration {0} mu={1} delta={2} deltamu={3} resb={4}".format(numiter, mu, deltab, deltab*mu, b))
+
+            numiter+=1
+
+            condition=False
+            for i in range (len(b)):
+                if math.fabs ((b[i]-bpriv[i])/bpriv[i])>math.pow(10,-1*_NSIG):
+                    condition=True
+
+
+            if numiter>3000: #max number of iterations
+                log+="GKNUX1: Break due to max number of iteration exceed"
+                break
+
+        if len(Sklist)>100:
+            ll=[Sklist[i] for i in range(0,len(Sklist),10)]
+            Sklist = ll
+
+        return {'b':b, 'numiter':numiter, 'log':log, 'Sklist':Sklist, 'Sk':Sk}
+
+
+
+class EstimatorDecorator(AbstractEstimator):
+    def __init__(self, bstart, bend, xstart, xend, model, measdata, Ve, component:AbstractEstimator):
+        AbstractEstimator.__init__(bstart, bend, xstart, xend, model, measdata, Ve)
+        self.component = component
+
+    def estimate(self):
+        #поведение по умолчанию
+        return self.component.estimate()
+
+
--- Fianora/Fianora_Models.py
+++ Fianora/Fianora_Models.py
@@ -1,15 +1,31 @@
 __author__ = 'vasilev_is'
+import math
+from scipy import optimize
+import numpy as np
+
+# абстракции из данного раздела можно раскрутить и для автоматизированного нахождения производных символьным методом
+
+
 
 class AbstractModel:
+    """
+    Defines a model abstraction.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
     def funcf (self, x,b):
+        """
+        Main model function
+        """
         raise NotImplementedError("funcf Please Implement this method")
 
-    def jacf(self, x, b, y):
+    def jacf(self, x, b, y=None):
+        """
+        Jacobian
+        """
         raise NotImplementedError("jacf Please Implement this method")
-
-    def hess(self, x, b, y):
-        raise NotImplementedError("hess Please Implement this method")
-
 
     def cal_funcf(self):
         return self.funcf
@@ -17,25 +33,205 @@ class AbstractModel:
     def cal_jacf(self):
         return self.jacf
 
-    def cal_hess(self):
-        return self.hess
+
 
 
 class SemiconductorModel(AbstractModel):
     FT = 0.02586419
 
 
-class SimpleDiodeModel(SemiconductorModel):
-    pass
+class ImplicitModel(AbstractModel):
+    """
+    Defines model class, which model defined implicitly, defining 2 abstract functions: mf, which
+    is model and dfdy and dfdb.
+    Then it implements solver, and jacobian, as template methods (implemented method which use abstract methods)
+    """
+
+    def dfdy(self, y, x, b):
+        raise NotImplementedError("hess Please Implement this method")
+
+    def mf(self, y, x, b):
+        raise NotImplementedError("hess Please Implement this method")
+
+    def dfdb(self, y, x, b):
+        raise NotImplementedError("hess Please Implement this method")
+
+    def funcf(self, x, b):
+
+        solvinit=[1]
+
+        try:
+            solx=optimize.root(self.mf, solvinit, args=(x,b,c), jac=self.dfdy, method='lm').x
+            #http://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
+            #http://codereview.stackexchange.com/questions/28207/is-this-the-fastest-way-to-find-the-closest-point-to-a-list-of-points-using-nump
+        except BaseException as e:
+            print ('solving function: Error in findroot=',e)
+            return None
+
+        if solx-solvinit==[0]*len(solx):
+            return None
+
+        return solx
+
+    def jacf(self, x, b, y=None):
+
+        # если значение y не приходит, то мы его находим вызовом солвера
+        # кстати эта часть одинакова, с другой стороны,
+        # в тех функциях, где модель задаётся явно, y не участвует в производной и потому можно всегда передавать None
+        if not y:
+            y = self.funcf(x, b)
+        return np.dot(np.linalg.inv(self.dfdy(y, x, b)), self.dfdb(y, x, b))
 
 
-class AdvancedDiodeModel(SemiconductorModel):
+
+class SimpleDiodeModel(SemiconductorModel, ImplicitModel): # множественное наследование - от полупроводниковой модели и от неявной модели
+    def mf (self, y,x,b):
+        # модельная функция
+        mm=float(b[0]*(math.exp((x[0]-y[0]*b[2])/(self.FT*b[1])) -1)-y[0])
+        return [mm]
+
+    def dfdy(self,y,x,b):
+        return np.array ([[ -1 - b[0]*b[2]*math.exp((-b[2]*y[0] + x[0])/(self.FT*b[1]))/(self.FT*b[1])]])
+
+    def dfdb(self,y,x,b):
+        return np.matrix( [ [math.exp((-b[2]*y[0] + x[0])/(self.FT*b[1])) - 1,
+                                          -b[0]*(-b[2]*y[0] + x[0])*math.exp((-b[2]*y[0] + x[0])/(self.FT*b[1]))/(self.FT*b[1]**2),
+                                          -b[0]*y[0]*math.exp((-b[2]*y[0] + x[0])/(self.FT*b[1]))/(self.FT*b[1])]])
+
+
+
+class AdvancedDiodeModel(SemiconductorModel, ImplicitModel):
     pass
 
 
 class EMTransistorModel(SemiconductorModel):
-    pass
+    def funcf(self, x,b):
+        """
+        Модель Эберса-Молла явная
+        :param x:
+        :param b:
+        :return:
+        """
+        b = list(map(float,b))
+        x = list(map(float,x))
+
+        Vbe = -1*x[0]
+        Vbc = -1*x[1]
 
 
-class GPTransistorModel(SemiconductorModel):
-    pass
+        IS = b[0]       # сквозной ток насыщения   1e-16
+        BF = b[1]       # максимальное значение нормального коэфф усиления по току с ОЭ 100
+        VAF = b[2]      # прямое напряжение Эрли    inf
+        VAR = b[3]      # инверсное напряжение Эрли inf
+        IKF =  b[4]     # ток перехода к высококу уровню инжекции inf
+        ISE = b[5]      # генерационно-рекомбинационный ток насыщения  эмиттерного перех 0
+        NE = b[6]       # коэфф. неидеальности ген-рек тока эмиттерного перех   1
+        NR = b[7]       # коэфф неидеальности для диффузного тока в инв режиме  1
+        NF = b[8]       # коэфф неидеальности для диффузионного тока в нормальном режиме        1
+        NC = b[9]       # коэфф неидеальности генерационно-рекомбинацоинного тока коллектора    1
+        BR = b[10]      # инверсный коэфф усиления тока в схеме с ОЭ 1
+        IKR = b[11]     # ток перехода к высокому уровню инжекции в инверсном включении inf
+        ISC = b[12]     # генерационно-рекомбинационный ток насыщения колекторного перех 0
+        RE = b[13]      # сопротивление эмиттера
+        RC = b[14]      # сопротивление коллектора
+        RB = b[15]      # сопротивление базы
+
+
+        FT = self.FT
+
+        Icc = IS * (math.exp(Vbe/(NF*FT))-math.exp(Vbc/(NR*FT) ))
+        Ibe = (IS/BF) * (math.exp(Vbe/(NF*FT))-1)
+        Ibc = (IS/BR) * (math.exp(Vbc/(NR*FT))-1)
+        # генерационно-рекомбинационные составляющие
+
+        Ie = Icc+Ibe
+        Ic = Ibc-Icc
+        Ib = Ie-Ic
+
+        #Ie = IS * (math.exp(Vbe/(NF*FT))-math.exp(Vbc/(NR*FT) )) +  (IS/BF) * (math.exp(Vbe/(NF*FT))-1)
+        #Ic = IS * (math.exp(Vbe/(NF*FT))-math.exp(Vbc/(NR*FT) )) + (IS/BR) * (math.exp(Vbc/(NR*FT))-1)
+        #ток базы нам не требуется, хотя..
+
+        y=[Ie, Ic, Ib]
+        return y
+
+
+
+
+class GPTransistorModel(SemiconductorModel, ImplicitModel):
+    def funcf(self, x, b):
+        """
+        Модель Гуммеля-Пуна
+        :param x:
+        :param b:
+        :return:
+        """
+        import mpmath as mpm
+
+        FT=self.FT
+
+        #pr = lambda  x: mpm.mpf(str(x))
+        pr=float
+
+        b = list(map(pr  ,b))
+        x = list(map(pr ,x))
+
+
+        Vbe = -1*x[0]
+        Vbc = -1*x[1]
+        Vbc = -1*x[1]
+
+
+
+
+        IS = b[0]       # сквозной ток насыщения   1e-16
+        BF = b[1]       # максимальное значение нормального коэфф усиления по току с ОЭ 100
+        VAF = b[2]      # прямое напряжение Эрли    inf
+        VAR = b[3]      # инверсное напряжение Эрли inf
+        IKF =  b[4]     # ток перехода к высококу уровню инжекции inf
+        ISE = b[5]      # генерационно-рекомбинационный ток насыщения  эмиттерного перех 0
+        NE = b[6]       # коэфф. неидеальности ген-рек тока эмиттерного перех   1
+        NR = b[7]       # коэфф неидеальности для диффузного тока в инв режиме  1
+        NF = b[8]       # коэфф неидеальности для диффузионного тока в нормальном режиме        1
+        NC = b[9]       # коэфф неидеальности генерационно-рекомбинацоинного тока коллектора    1
+        BR = b[10]      # инверсный коэфф усиления тока в схеме с ОЭ 1
+        IKR = b[11]     # ток перехода к высокому уровню инжекции в инверсном включении inf
+        ISC = b[12]     # генерационно-рекомбинационный ток насыщения колекторного перех 0
+        RE = b[13]      # всякоразные сопротивления
+        RC = b[14]
+        RB = b[15]
+
+        # главные составляющие
+        Qfrac = .5*(1+Vbc/VAF+Vbe/VAR)+(.25*(1+Vbc/VAF+Vbe/VAR)**2+(IS/IKF)*(mpm.exp(Vbe/(NF*FT))-1)+(IS/IKR)*(mpm.exp(Vbc/(NR*FT))-1 )   )**.5
+
+        Qfrac = 1/Qfrac
+
+        Icc = Qfrac * IS * (mpm.exp(Vbe/(NF*FT))-mpm.exp(Vbc/(NR*FT) ))
+
+
+
+        Ibe = (IS/BF) * (mpm.exp(Vbe/(NF*FT))-1)
+        Ibc = (IS/BR) * (mpm.exp(Vbc/(NR*FT))-1)
+
+        # генерационно-рекомбинационные составляющие
+        Ire = ISE*(mpm.exp(Vbe/(NE*FT))-1)
+
+
+
+        Irc = ISC*(mpm.exp(Vbc/(NC*FT))-1)
+
+        if (x[1]<0):
+            Irc=0
+            Ibc*=2
+
+
+
+        Ie = Icc+Ibe+Ire
+
+        Ic = Ibc+Irc-Icc
+        #print (Ibc,Irc,Icc)
+
+
+        Ib = Ie-Ic
+        y=[Ie, Ic, Ib]
+        return y
