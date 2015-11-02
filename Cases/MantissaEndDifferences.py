cc4db1e86ab21cc6626774b869afb869a6234699
Reiner <mksd@list.ru>
2015-11-02 14:41:56 +0300

    213213123

--- Cases/MantissaEndDifferences.py
+++ Cases/MantissaEndDifferences.py
@@ -66,7 +66,10 @@ def transform_iia_to_matrix (iia, index):
 
 
     for i in range (iia.ll()):
-        mat[i*2][ 0] = abs(iia[i]['b'][0])
+        #mat[i*2][0] = abs(iia[i]['b'][index])
+        mat[i*2][0] = iia[i]['b'][index]
+
+
 
     return makeEndDifferencesOnMatrix(mat,1)
 
@@ -87,13 +90,19 @@ def makeEndDifferencesOnMatrix(mat, squeeze=False):
         #start=1 if col&1 else 0;
         start = col
         for row in range (start, mat.shape[0]-1-col, 2):
-            mat[row][col]=abs(mat[row+1][col-1]-mat[row-1][col-1])
+            #mat[row][col]=abs(mat[row+1][col-1]-mat[row-1][col-1])
+            mat[row][col]=mat[row+1][col-1]-mat[row-1][col-1]
+
+
+
 
     if (squeeze):
         """
         Вернуть список списков конечных разностей
         """
         return [ [mat[row][col] for row in range (col, mat.shape[0]-1-col,2) ]  for col in range(1, mat.shape[1]) ]
+
+
     return mat
 
 
@@ -133,19 +142,19 @@ def makeListOfListPerIteration(listoflistoflistofdiffs):
  #   print (res)
     return res
 
-def printListOfListPerIteration(olololist):
+def printListOfListPerIteration(olololist, file=None):
     """
     just prints it
     """
     # cycle per diff
     j=1
     for diff in olololist:
-        print ('Number of difference level: {0}'.format(j))
+        print ('Number of difference level: {0}'.format(j), file=file)
         i=0
         for iteration in diff:
-            print ('Difference {0} - {1}'.format(i, iteration))
+            print ('Difference {0} - {1}'.format(i, iteration), file=file)
             i+=1
-        print (" ")
+        print (" ", file)
         j+=1
 
     description = "See table of end differences, actually: number of difference level: \n" \
@@ -158,54 +167,206 @@ def printListOfListPerIteration(olololist):
 
 
 
-    print (description)
+    print (description, file=file)
 
 
-def makeAllEndDiffs (datafile):
+def makeAllEndDiffs (datafile, index):
     with open(datafile, 'rb') as f:
         rrlist = pickle.load(f)
         rrlist_filtered = [case for case in rrlist if case.ll()<30]
-        return [transform_iia_to_matrix(lst, 0) for lst in rrlist_filtered ]
+        return [transform_iia_to_matrix(lst, index) for lst in rrlist_filtered ]
+
+STAT_MEAN=0
+STAT_VAR=1
+STAT_SIGMA=2
+STAT_SKEW=3
+STAT_KURTOSIS=4
 
 
 
-def makeAverageListOutOfEndDifferencesList(listdiffs):
+def makeCharactOutOfEndDifferencesList(listdiffs, type=STAT_MEAN):
     """
+    type=0, mean
+    1, variance
+    2, sigma
     """
     import copy
     import statistics
-    lst = copy.copy(listdiffs)
+    import scipy.stats as scps
 
 
-    for j in lst:
-        for m in j:
-            for t in m:
-                t = statistics.mean(t)
+    lst = copy.deepcopy(listdiffs)
+
+    func={0:statistics.mean, 1:statistics.variance, 2:lambda x: math.sqrt(statistics.variance(x)), 3:scps.skewtest, 4:scps.kurtosistest  }
+
+    for j in range (len(lst)):
+        for k in range (len(lst[j])):
+            lst[j][k] = func[type](lst[j][k])
+
     return lst
 
-def makeDispOutOfEndDifferences(listdiffs):
-    """
-    """
-    pass
 
-def makeHistsOutOfEndDifferences(listdiffs):
-    """
 
-    """
 
-    pass
+
+def makeHistsOutOfEndDifferences(listdiffs, mainfolder, limy=200):
+    """
+    limy - end value oy
+    """
+    import os, os.path
+
+    import matplotlib.pyplot as plt
+    # os.makedirs(path[, mode])¶
+
+    for j in range (len(listdiffs)):  #для каждого уровня разности
+        fldpath = os.path.join(os.path.dirname(mainfolder), "diflevel_{0}".format(j)) #  получили путь к папке
+        os.makedirs(fldpath)
+
+        for m in range(len(listdiffs[j])):
+            fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
+            # x1,x2,y1,y2 = plt.axis()
+            # plt.axis((x1,x2,y1,limy))
+
+            #   axes = plt.gca()
+            #axes.set_ylim([0,200])
+
+
+            #ax.hist( [x for x in listdiffs[j][m] if x<.003] ,30)
+            ax.hist( [x for x in listdiffs[j][m] ] ,30)
+
+
+
+            #это у нас графики значений компонента вектора по итерациям.
+            imgpath = os.path.join(fldpath, "img_{0}.png".format(m))
+            fig.savefig(imgpath)
+            plt.close(fig)
+
+
+
+
+
+
+from PyQt5.QtWidgets import QApplication, QWidget,  QMainWindow
+from PyQt5 import uic
+
+
+from uifiles.Mantissa2UI.m2mainwindow import Ui_MainWindow
+
+
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.push1.clicked.connect(self.handle_button_1)
+
+
+    def handle_button_1(self):
+        self.textConsole.append("Button1 pressed")
 
 
 
 def test():
 
-    datafile = "resfiles/resdump.dat"
-    a = makeAllEndDiffs(datafile)
+    import os.path
+
+    datafile = "resfiles/resdump205_DISP.dat"
+    #datafile = "resfiles/resdump205.dat"
+
+    #folder = "resfiles/hist_all_end_diffs_limhists/"
+
+    folder = "resfiles/hists_all_end_diffs/"
+
+
+    b = makeAllEndDiffs(datafile,2) # 0 - index of a variable, this case b0
+
+
+    a = makeListOfListPerIteration(b)
+
+
+
+
+    #
+    # with open(os.path.join(folder,'average.txt'), 'wt') as f:
+    #     printListOfListPerIteration(makeCharactOutOfEndDifferencesList(a,STAT_MEAN), f)
+    #
+    # with open(os.path.join(folder,'variance.txt'), 'wt') as f:
+    #         printListOfListPerIteration(makeCharactOutOfEndDifferencesList(a,STAT_VAR), f)
+    #
+    # with open(os.path.join(folder,'sigma.txt'), 'wt') as f:
+    #         printListOfListPerIteration(makeCharactOutOfEndDifferencesList(a,STAT_SIGMA), f)
+
+    #makeHistsOutOfEndDifferences(a, folder, limy=200)
+    #makeHistsOutOfEndDifferences(a, folder)
+
+# NOW WE TEST FOR NORMALITY
+
+    import scipy.stats as scps
+
+
+    import random
+    random.seed()
+
+    normlist = [random.normalvariate(10, 2) for x in range(200)]
+
+    print (scps.skewtest(normlist))
+    print (scps.kurtosistest(normlist))
+    print (scps.normaltest(normlist))
+
+
+
+
+    exit(0)
+
+
+
+    with open(os.path.join(folder,'skew.txt'), 'wt') as f:
+        printListOfListPerIteration(makeCharactOutOfEndDifferencesList(a,STAT_SKEW), f) #  critical 0.34  http://mvpprograms.com/help/mvpstats/distributions/SkewnessCriticalValues
+
+    with open(os.path.join(folder,'kurtosis.txt'), 'wt') as f:
+        printListOfListPerIteration(makeCharactOutOfEndDifferencesList(a,STAT_KURTOSIS), f) #critical low -0.54	high 0.79  http://mvpprograms.com/help/mvpstats/distributions/KurtosisCriticalValues
+
+
+# http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3591587/
+
+    #https://onlinecourses.science.psu.edu/statprogram/node/138
+
+
+
+
+
+
+
+# we must test got distribution for normality
+# http://mvpprograms.com/help/mvpstats/distributions/KurtosisCriticalValues
+
+# http://mvpprograms.com/help/mvpstats/contents
+
+#http://mvpprograms.com/help/mvpstats/distributions/SkewnessCriticalValues
+
+
+
+
+
+
+
+
+    # app = QApplication(sys.argv)
+    # w = MainWindow()
+    # w.show()
+    # sys.exit(app.exec_())
+
+
+
+
 
 #    [print (r) for r in a]
 
-    b = makeListOfListPerIteration(a)
-    printListOfListPerIteration(b)
+    #printListOfListPerIteration(makeAverageListOutOfEndDifferencesList(b))
+
+
+
 
 
 
